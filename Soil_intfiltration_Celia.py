0db5051a6d01d69ca4d04690dcc1ace0653aaf5e
Celia Trunz <celia.trunz@gmail.com>
2020-08-18 20:01:11 -0600

    Update Soil_intfiltration_Celia.py

--- Soil_intfiltration_Celia.py
+++ Soil_intfiltration_Celia.py
@@ -12,23 +12,26 @@ from landlab.io.esri_ascii import read_esri_ascii
 from landlab.plot.imshow import imshow_grid
 from landlab.components import SoilInfiltrationGreenAmpt
 
-#Create grid with topography
-mg, z = read_esri_ascii('Square_TestBasin.asc', name='topographic__elevation')
-mg.set_closed_boundaries_at_grid_edges(True, True, True, True)
-imshow_grid(mg, 'topographic__elevation')
+# #Create grid with topography
+# sq_basin_mg, z = read_esri_ascii('Square_TestBasin.asc', name='topographic__elevation')
+# sq_basin_mg.set_closed_boundaries_at_grid_edges(True, True, True, True)
+# imshow_grid(sq_basin_mg, 'topographic__elevation')
 
-# #Create simple grid (from example)
-# mg = RasterModelGrid((4,3), xy_spacing=10.)
+#Create simple grid (from example)
+mg = RasterModelGrid((4,3), xy_spacing=10.)
 
 hydraulic_conductivity = mg.ones('node')*1.e-6
 hydraulic_conductivity.reshape((4,3))[0:2,:] *= 10000.
+
+
 
 h = mg.add_ones("surface_water__depth", at="node")
 h *= 0.01
 d = mg.add_ones("soil_water_infiltration__depth", at="node", dtype=float)
 d *= 0.2
 SI = SoilInfiltrationGreenAmpt(
-    mg,hydraulic_conductivity=hydraulic_conductivity)
+    mg,hydraulic_conductivity=hydraulic_conductivity,
+    soil_type ='silt loam')
 
 for i in range(10):  # 100s total
     SI.run_one_step(10.)
@@ -48,7 +51,11 @@ swid = mg.at_node['soil_water_infiltration__depth']
 
 #SoilInfiltrationGreenAmpt.input_var_names
 plot_graph(mg, at="node")
+
 imshow_grid(mg, 'surface_water__depth')
 
+imshow_grid(mg, 'soil_water_infiltration__depth')
 
+#%%
+hugo_mg, z = read_esri_ascii('hugo_site.asc', name='topographic__elevation')
 
